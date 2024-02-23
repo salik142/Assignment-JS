3afe6a2f297a08200c1719e95b021de0a878f399
salik142 <159442014+salik142@users.noreply.github.com>
2024-02-23 09:59:59 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,63 @@
+//post increament
+
+var counter = 1;
+counter++;
+console.log(counter);
+
+//post decreament
+var counter = 100;
+counter = counter - 2; //98
+counter--;
+
+var _name = "salik";
+var fathername = "sohail";
+var fullname = _name + fathername;
+console.log(fullname);
+
+//less than
+var number1 = 10;
+var number2 = 20;
+var isNum1IsGreaterThanNum2 = number1 > number2;
+console.log(isNum1IsGreaterThanNum2);
+
+alert("Hello World");
+
+var x = "favourite food";
+var num1 = 2;
+var num2 = 4;
+var legalName = 'legal-name';
+var num1 = 2;
+num1++;
+var h = 1;
+h++;
+h++;
+
+var y = 4 * 3 / (3 * (8 + 5));
+var mod = 7 % 2;
+
+
+
+var y = 1 / 11;
+num2++;
+num2++;
+
+var b = 2 + num1;
+console.log(t);
+
+var n = b + legalName;
+console.log(u);
+
+var q = prompt("Give any number");
+console.log(q);
+
+var number = prompt(
+  "Give Me A Number I Will Tell You Your Number Is Even Or Odd"
+);
+var remainder = number % 2;
+var message = "";
+if (remainder == 0) {
+  message = "your number is even";
+} else {
+  message = "your number is odd";
+}
+alert(message);
